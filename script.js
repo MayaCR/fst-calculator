511e2620c5030baa6970325c77733b7fe133bb1b
Maya <106397404+MayaChena@users.noreply.github.com>
2023-08-13 03:57:40 -0400

    Create function update display

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-let firstNum = 0, secondNum = 0, operator = '';
+const display = document.querySelector('.numbers');
+const buttons = document.querySelectorAll('.button')
+let firstNum = 0; 
+let secondNum = 0; 
+let operator = '';
+let displayNumber = 0;
 
 function addition (firstNum, secondNum) {
-    return firstNum + secondNum
-}
+    return firstNum + secondNum;
+};
 
 function subtraction (firstNum, secondNum) {
-    return firstNum - secondNum
-}
+    return firstNum - secondNum;
+};
 
 function multiply (firstNum, secondNum) {
-    return firstNum * secondNum
-}
+    return firstNum * secondNum;
+};
 
 function divide (firstNum, secondNum) {
-    return firstNum / secondNum
-}
+    return firstNum / secondNum;
+};
 
 function operate (firstNum, operator, secondNum) {
     return operator === '+' ? addition(firstNum, secondNum) :
     operator === '-' ? subtraction(firstNum, secondNum) :
     operator === '*' ? multiply(firstNum, secondNum) :
-    operator === '/' ? divide(firstNum, secondNum) : 'Error'
-}
+    operator === '/' ? divide(firstNum, secondNum) : 'Error';
+};
 
-console.log(operate(1, '+', 4))
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        display.textContent += e.target.value;
+    });
+});
+
+console.log(operate(1, '+', 4));
 
 
