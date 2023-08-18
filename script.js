c7e5bd6fcc124fe170bd853c32e707e7886a1cfa
Maya <106397404+MayaChena@users.noreply.github.com>
2023-08-18 09:10:06 -0400

    Changed functions associated with event listeners

--- script.js
+++ script.js
@@ -1,54 +1,92 @@
 const display = document.querySelector('.numbers');
 const buttons = document.querySelectorAll('.button');
-const clearButton = document.querySelector('#clear')
-const equalButton = document.querySelector('#equal')
-let firstNum = 0; 
-let secondNum = 0; 
-let operator = '';
-let displayNumber = 0;
 
-function addition (firstNum, secondNum) {
+let firstNum = null; 
+let secondNum = null; 
+let operator = null;
+let displayNumber = '';
+
+function addition(firstNum, secondNum) {
     return firstNum + secondNum;
 };
 
-function subtraction (firstNum, secondNum) {
+function subtraction(firstNum, secondNum) {
     return firstNum - secondNum;
 };
 
-function multiply (firstNum, secondNum) {
+function multiply(firstNum, secondNum) {
     return firstNum * secondNum;
 };
 
-function divide (firstNum, secondNum) {
+function divide(firstNum, secondNum) {
     return firstNum / secondNum;
 };
 
-function operate (firstNum, operator, secondNum) {
-    return operator === '+' ? addition(firstNum, secondNum) :
-    operator === '-' ? subtraction(firstNum, secondNum) :
-    operator === '*' ? multiply(firstNum, secondNum) :
-    operator === '/' ? divide(firstNum, secondNum) : 'Error';
+function calculate(firstNum, operator, secondNum) {
+    let solution;
+
+    switch (operator) {
+        case 'add':
+            solution = addition(firstNum, secondNum);
+            break;
+        case 'subtract':
+            solution = subtraction(firstNum, secondNum);
+            break;
+        case 'multiply':
+            solution = multiply(firstNum, secondNum);
+            break;
+        case 'divide':
+            solution = divide(firstNum, secondNum);
+            break;
+        default: 
+            'Error'
+    }; 
+    return solution = displayNumber;
 };
 
 function updateDisplay() {
-
+    display.textContent = displayNumber;
 }
+
+function getValues() {
+    if (firstNum === null) {
+        firstNum = displayNumber;
+        displayNumber = '';
+    } else if (firstNum !== null) {
+        secondNum = displayNumber;
+        updateDisplay();
+    };
+};
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
-        display.textContent += e.target.value;
-        if (e.target.id) {
-            displayNumber = display.textContent;
-            display.textContent = '';
+        displayNumber += e.target.value;
+        updateDisplay();
+        
+        operator = e.target.id;
+        
+        let solution;
+        
+        switch(operator) {
+            case 'add':
+                getValues()
+                console.log('firstNum: ' + firstNum);
+                console.log(operator)
+                console.log('secondNum: ' + secondNum);
+                solution = calculate(firstNum, operator, secondNum);
+                console.log(solution)
+                break;
+            case 'equal':
+                solution = calculate(firstNum, operator, secondNum);
+                console.log(solution);
+                break;
+            case 'clear':
+                displayNumber = '';
+                firstNum = null;
+                secondNum = null;
+                operator = null;
+                updateDisplay();
+                break;
         };
     });
 });
-
-clearButton.addEventListener('click', () => {
-    display.textContent = '';
-    displayNumber = 0;
-});
-
-console.log(operate(1, '+', 4));
-
-
